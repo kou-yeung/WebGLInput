2ce48be538845460d9a637d7c92097b98c2af82c
kou_yeung <kou_yeung@hotmail.com>
2020-05-08 18:02:27 +0900

    TMPの点滅軽減

--- Assets/WebGLSupport/WebGLInput/Wrapper/WrappedTMPInputField.cs
+++ Assets/WebGLSupport/WebGLInput/Wrapper/WrappedTMPInputField.cs
@@ -133,15 +133,24 @@ namespace WebGLSupport
                 // HACK : 1フレーム無効にする
                 // MEMO : 他にいい方法Rebuildがあれば対応する
                 // LayoutRebuilder.ForceRebuildLayoutImmediate(); で試してダメでした
-                input.textComponent.enabled = false;
+                input.textComponent.enabled = rectOverlaps(input.textComponent.rectTransform, input.textViewport);
                 input.textComponent.SetAllDirty();
                 input.Rebuild(CanvasUpdate.LatePreRender);
+                //Debug.Log(input.textComponent.enabled);
             }
             else
             {
                 input.textComponent.enabled = true;
             }
 #endif
+        }
+
+        bool rectOverlaps(RectTransform rectTrans1, RectTransform rectTrans2)
+        {
+            Rect rect1 = new Rect(rectTrans1.localPosition.x, rectTrans1.localPosition.y, rectTrans1.rect.width, rectTrans1.rect.height);
+            Rect rect2 = new Rect(rectTrans2.localPosition.x, rectTrans2.localPosition.y, rectTrans2.rect.width, rectTrans2.rect.height);
+
+            return rect1.Overlaps(rect2);
         }
     }
 }
