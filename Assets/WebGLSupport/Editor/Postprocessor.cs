bc79d24b017c7e08b9f644b65d8fb8a70cbaf130
kou_yeung <kou_yeung@hotmail.com>
2024-05-15 09:08:00 +0900

    overwrite default temp fullscreen button

---
+++ Assets/WebGLSupport/Editor/Postprocessor.cs
@@ -0,0 +1,57 @@
+using System.IO;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+namespace WebGLSupport
+{
+    public class Postprocessor
+    {
+        const string MenuPath = "Assets/WebGLSupport/OverwriteFullscreenButton";
+
+#if UNITY_2021_1_OR_NEWER
+        static readonly bool supportedPostprocessor = true;
+        static readonly string defaultFullscreenFunc = "unityInstance.SetFullscreen(1);";
+        static readonly string fullscreenNode = "unity-container";
+#else
+        static readonly bool supportedPostprocessor = false;
+        static readonly string defaultFullscreenFunc = "";
+        static readonly string fullscreenNode = "";
+#endif
+
+        [PostProcessBuild(1)]
+        public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+        {
+            if (target != BuildTarget.WebGL) return;
+            if (!supportedPostprocessor) return;
+            if (!Menu.GetChecked(MenuPath)) return;
+
+            var path = Path.Combine(pathToBuiltProject, "index.html");
+            if (!File.Exists(path)) return;
+
+            var html = File.ReadAllText(path);
+
+            // check node is exist
+            if (html.Contains(fullscreenNode))
+            {
+                html = html.Replace(defaultFullscreenFunc, $"document.makeFullscreen('{fullscreenNode}');");
+                File.WriteAllText(path, html);
+            }
+        }
+
+        [MenuItem(MenuPath)]
+        public static void OverwriteDefaultFullscreenButton()
+        {
+            var flag = !Menu.GetChecked(MenuPath);
+            Menu.SetChecked(MenuPath, flag);
+            PlayerPrefs.SetInt(MenuPath, flag ? 1 : 0);
+        }
+
+        [MenuItem(MenuPath, validate = true)]
+        private static bool OverwriteDefaultFullscreenButtonValidator()
+        {
+            Menu.SetChecked(MenuPath, PlayerPrefs.GetInt(MenuPath, 1) == 1);
+            return true;
+        }
+    }
+}
